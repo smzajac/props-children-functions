77e7962e113b6a8d62eb503e0774851838354925
Sarah Zajac <sarahmzajac@gmail.com>
2017-09-18 16:58:41 -0400

    3rd commit

--- src/components/App.js
+++ src/components/App.js
@@ -49,27 +49,26 @@ class ParentComponent extends Component {
   handleInput(e) {
     e.preventDefault();
     //set the state on input change
-    this.setState({whatToSay: this.state.whatToSay});
+    this.setState({whatToSay: e.target.value});
   }
   handleSubmit(e) {
     e.preventDefault();
     //check console to see if firing
     console.log("fired");
     //set the state for props and for value (prevents output from appearing when typing)
-    this.setState({whatToSay: this.state.whatToSay, whatWasSaid: this.state.whatToSay});
+    this.setState({whatToSay: '', whatWasSaid: this.state.whatToSay});
     //clear our input by resetting state
-    this.setState({whatToSay: ""});
 
   }
   render() {
     return (
-      <div><p>'Smart Component: I have a function, but something isnt working? I also need to pass that function to the ChildComponent.'</p>
+      <div>
         <div>
-          <input onChange={this.handleInput} type="text" placeholder="Say It, Don't Spray It!" />
+          <input value ={this.state.whatToSay} onChange={this.handleInput} type="text" placeholder="Say It, Don't Spray It!" />
         </div>
         <div>
           <ChildComponent onClick={this.handleSubmit}/>
-          <DisplayComponent sayWhat={this.state.whatToSay} />
+          <DisplayComponent sayWhat={this.state.whatWasSaid} />
         </div>
       </div>
     );
